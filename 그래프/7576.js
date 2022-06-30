b08047e663e6c01b1ed13480a631debc30c15d51
M.W <auddnjs2008@naver.com>
2022-06-30 23:31:42 +0900

    그래프

--- 그래프/7576.js
+++ 그래프/7576.js
@@ -18,12 +18,54 @@ const directions = [
   [0, 1],
   [0, -1],
 ];
+
 let day = 0;
 
-const bfs = (node) => {
-  const queue = [node];
-
+const bfs = (queue) => {
   while (queue.length) {
-    const [curX, curY] = queue.shift();
+    const [curX, curY, cnt] = queue.shift();
+    day = Math.max(day, cnt);
+    for (let i = 0; i < 4; i++) {
+      const [mX, mY] = directions[i];
+      const afterX = curX + mX;
+      const afterY = curY + mY;
+
+      if (
+        isValid(afterX, afterY) &&
+        !visited[afterY][afterX] &&
+        box[afterY][afterX] === 0
+      ) {
+        visited[afterY][afterX] = true;
+        box[afterY][afterX] = 1;
+        queue.push([afterX, afterY, cnt + 1]);
+      }
+    }
   }
 };
+const queue = [];
+let zero = false;
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (box[i][j] === 1) {
+      queue.push([j, i, 0]);
+      visited[i][j] = true;
+    } else if (box[i][j] === 0) {
+      zero = true;
+    }
+  }
+}
+
+if (!zero) {
+  day = 0;
+} else {
+  bfs(queue);
+  for (let i = 0; i < N; i++) {
+    if (box[i].includes(0)) {
+      day = -1;
+      break;
+    }
+  }
+}
+
+console.log(day);
